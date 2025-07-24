200ff19571f49e2e1f1216ed3cdeb6464730184f
Léo Pires <leopiresss@gmail.com>
2025-07-24 14:11:25 +0000

    v1.5 2025-07-24

--- ias/ambiente-instalar.sh
+++ ias/ambiente-instalar.sh
@@ -1,11 +1,17 @@
+#Instala o csadvisor e o dashboard do Microk8s
+# é utilizado o cadvisor.yml como pod
+# o token gerado pelo script deve ser utilizado na interface do dashboard
 
+echo "⚙️ Gerando o token do dashboard..." 
 
-echo "⚙️ Gerando o token do dashboard.../n" 
 microk8s kubectl describe secret -n kube-system microk8s-dashboard-token 
 
 echo "⚙️ Ativando o cadvisor para métricas..." 
+
 kubectl apply -f cadvisor.yml
+
 echo "Passo 1: Listando pods do cAdvisor no namespace kube-system..."
+
 CADVISOR_POD=$(kubectl get pods -n kube-system | awk '/cadvisor/ {print $1}')
 
 if [[ -z "$CADVISOR_POD" ]]; then
@@ -17,7 +23,8 @@ echo "Pod do cAdvisor encontrado: $CADVISOR_POD"
 
 echo
 echo "Passo 2: Realizando port-forward local da porta 8080 do pod $CADVISOR_POD..."
-kubectl port-forward -n kube-system pod/$CADVISOR_POD 8080:8080 > /dev/null 2>&1 &
+
+kubectl port-forward --address 0.0.0.0 -n kube-system pod/$CADVISOR_POD 8080:8080 > /dev/null 2>&1 &
 PF_PID=$!
 sleep 3  # Aguarda o port-forward estabilizar
 
@@ -32,6 +39,6 @@ curl --silent http://localhost:8080/metrics | head -20
 
 
 echo "⚙️ Disponibilizando o dashboard do Microk8s.../n" 
-nohup microk8s kubectl port-forward --address 0.0.0.0 -n kube-system service/kubernetes-dashboard 10443:443 
+microk8s kubectl port-forward --address 0.0.0.0 -n kube-system service/kubernetes-dashboard 10443:443 > /dev/null 2>&1 & 
  
 
--- ias/info-k8s.sh
+++
@@ -1,9 +0,0 @@
-
-#!/bin/bash
-
-for ctx in $(kubectl config get-contexts -o name); do
-  echo "Cluster: $ctx"
-  kubectl --context $ctx get nodes -o custom-columns=NAME:.metadata.name,TIPO_AMBIENTE:.metadata.labels.tipo_de_ambiente
-  kubectl --context $ctx top node --no-headers | awk '{print "Nó:", $1, "- CPU:", $2, "- Memória:", $4}'
-  echo "---------------------------------------"
-done
