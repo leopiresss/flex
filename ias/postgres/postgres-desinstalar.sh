999c7a72e77ab8db98963385f7fa89cdc3aa4ebf
Léo Pires <leopiresss@gmail.com>
2025-09-07 22:09:07 -0300

    v7 dados tabulados

--- ias/postgres/postgres-desinstalar.sh
+++ ias/postgres/postgres-desinstalar.sh
@@ -2,6 +2,7 @@
 kubectl delete deployment postgres-deployment -n stress-app
 kubectl delete service postgres-service -n stress-app
 kubectl delete configmap postgres-config -n stress-app
-kubectl delete pvc postgres-pv-pvc -n stress-app
+kubectl delete pvc postgres-pvc -n stress-app
+kubectl delete pv postgres-pv -n stress-app
 
 
--- ias/prometheus/instalar-prometheus.sh
+++ ias/prometheus/instalar-prometheus.sh
@@ -2,24 +2,49 @@
 #instalando port-forward do cAdvisor
 source ~/flex/lib/port-lib.sh 
 
-
+echo " ⚠️ -  Criando namespace monitoring..."
 kubectl create namespace monitoring
+
+echo " ⚠️ -  Aplicando configuração do Prometheus..."
 kubectl apply  -f prometheus-config.yaml -n monitoring
+
+echo " ⚠️ -  Aplicando autenticação do Prometheus..."
 kubectl apply -f prometheus-autenticacao.yaml -n monitoring
+
+echo " ⚠️ -  Aplicando deployment do Prometheus..."
 kubectl apply -f prometheus-deployment.yaml -n monitoring
+
+echo " ⚠️ -  Aplicando service do Prometheus..."
 kubectl apply -f prometheus-service.yaml -n monitoring
 
-# Verificar pods
+echo " ⚠️ -  Aplicando autenticação do Prometheus..."
+kubectl apply -f prometheus-autenticacao.yaml -n monitoring
+
+echo " ⚠️ -  Aplicando StorageClass do Alertmanager..."
+kubectl apply -f alertmanager-storageclass.yaml -n monitoring
+
+echo " ⚠️ -  Aplicando PersistentVolume do Alertmanager..."
+kubectl apply -f alertmanager-pv.yaml -n monitoring
+
+echo " ⚠️ -  Aplicando StatefulSet do Alertmanager..."
+kubectl apply -f alertmanager-statefulset.yaml -n monitoring
+
+echo " ⚠️ -  Aplicando ConfigMap do Alertmanager..."
+kubectl apply -f configmap-alertmanager.yaml -n monitoring
+
+echo " ⚠️ -  Aplicando service do Prometheus (segunda vez)..."
+kubectl apply -f prometheus-service.yaml -n monitoring
+
+echo " ⚠️ -  Verificando pods no namespace monitoring..."
 kubectl get pods -n monitoring
 
-# Verificar serviços
+echo " ⚠️ -  Verificando serviços no namespace monitoring..."
 kubectl get svc -n monitoring
 
+echo " ⚠️ -  Aguardando pods do Prometheus ficarem prontos..."
 microk8s kubectl wait --for=condition=ready pod -l app=prometheus -n monitoring --timeout=500s
 
-
 # kubectl port-forward --address 0.0.0.0 svc/prometheus-service 9090:9090 -n monitoring &
-
 
 echo "✅ Instalando conectividade"
 
@@ -34,8 +59,7 @@ PID_FILE="/tmp/cadivisor-pf.pid"
 NET_IFACE="ens33"
 RESOURCE_NOME_SISTEMA="PROMETHEUS"
 
-
-
+echo " ⚠️ -  Executando port-forward para o Prometheus..."
 # Chama a função principal da biblioteca, passando os argumentos necessários
 executar_port_forward \
     "$RESOURCE_TYPE" \
@@ -47,8 +71,3 @@ executar_port_forward \
     "$PID_FILE" \
     "$NET_IFACE" \
     "$RESOURCE_NOME_SISTEMA"
-
-
-
-
-
